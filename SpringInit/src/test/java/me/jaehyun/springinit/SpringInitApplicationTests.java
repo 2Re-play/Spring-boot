d5c09ce216145e07676eefb8e8662d8a3ff71b0f
2Re-play <wogusduddnjs@gmail.com>
2020-01-28 16:20:42 +0900

    springInit project first commit

---
+++ SpringInit/src/test/java/me/jaehyun/springinit/SpringInitApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jaehyun.springinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
